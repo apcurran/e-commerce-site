99d0ab6749e4fd553ac23fa3cda344b03e97c2ad
apc <alexcurran.va@gmail.com>
2020-07-07 12:46:15 -0500

    Added images and updated Product model

---
+++ models/Product.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const RatingsSchema = new mongoose.Schema({
+    user_id: { type: String, required: true },
+    user_rating: { type: Number, required: true }
+});
+
+const ProductSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    genre: { type: String, required: true },
+    price: { type: Number, min: 1, required: true },
+    description: { type: String, required: true },
+    img_path: { type: String, required: true },
+    ratings: [RatingsSchema]
+});
+
+module.exports = mongoose.model("Product", ProductSchema);
--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 "use strict";
 
+require('dotenv').config()
 const express = require("express");
 const PORT = process.env.PORT || 5000;
 const morgan = require("morgan");
 const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+
 const app = express();
 
 if (process.env.NODE_ENV !== "production") {
     app.use(morgan("dev"));
 }
+
+// DB Setup
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
 
 // Middleware
 app.set("view engine", "ejs");
