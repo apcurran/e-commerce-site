8207257948050131d45992a3c8261157ef548e2d
apc <alexcurran.va@gmail.com>
2020-07-08 15:47:38 -0500

    Index.ejs layout update to include partials for sidebar and grid with game items

--- routes/games-router.js
+++ routes/games-router.js
@@ -6,7 +6,59 @@ router.get("/", async (req, res) => {
     try {
         const products = await Product.find();
 
-        res.render("shop/index", { title: "Home", products });
+        res.render("shop/index", { title: "Home", products, genre: "all" });
+        
+    } catch (err) {
+        console.error(err);
+
+        res.render("shop/index", { title: "Home", error: err.message });
+    }
+});
+
+router.get("/games/action", async (req, res) => {
+    try {
+        const products = await Product.find({ genre: "action" });
+
+        res.render("shop/index", { title: "Home", products, genre: "action" });
+        
+    } catch (err) {
+        console.error(err);
+
+        res.render("shop/index", { title: "Home", error: err.message });
+    }
+});
+
+router.get("/games/adventure", async (req, res) => {
+    try {
+        const products = await Product.find({ genre: "adventure" });
+
+        res.render("shop/index", { title: "Home", products, genre: "adventure" });
+        
+    } catch (err) {
+        console.error(err);
+
+        res.render("shop/index", { title: "Home", error: err.message });
+    }
+});
+
+router.get("/games/rpg", async (req, res) => {
+    try {
+        const products = await Product.find({ genre: "rpg" });
+
+        res.render("shop/index", { title: "Home", products, genre: "rpg" });
+        
+    } catch (err) {
+        console.error(err);
+
+        res.render("shop/index", { title: "Home", error: err.message });
+    }
+});
+
+router.get("/games/sports", async (req, res) => {
+    try {
+        const products = await Product.find({ genre: "sports" });
+
+        res.render("shop/index", { title: "Home", products, genre: "sports" });
         
     } catch (err) {
         console.error(err);
