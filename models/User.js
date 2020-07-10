3599d827644aa510a1c08ff0cf8daad65ff60ed0
Alex Curran <alexcurran.va@gmail.com>
2020-07-10 13:02:26 -0500

    Added User model

---
+++ models/User.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    first_name: { type: String, min: 1, max: 200, required: true },
+    last_name: { type: String, min: 1, max: 200, required: true },
+    email: { type: String, min: 1, max: 100, required: true },
+    password: { type: String, min: 6, max: 100, required: true }
+});
+
+module.exports = mongoose.model("User", UserSchema);
