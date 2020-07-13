4f75c4adf4cf942b423a695526ae58725b4eafab
Alex Curran <alexcurran.va@gmail.com>
2020-07-13 14:49:28 -0500

    Added validate-user file and Joi validation functions

---
+++ validation/validate-user.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const Joi = require("@hapi/joi");
+
+function signupValidation(data) {
+    const schema = Joi.object({
+        first_name: Joi
+                .string()
+                .min(1)
+                .required(),
+        last_name: Joi
+                .string()
+                .min(1)
+                .required(),
+        email: Joi
+                .string()
+                .min(4)
+                .email()
+                .required(),
+        password: Joi
+                .string()
+                .min(6)
+                .required(),
+        admin_secret: Joi
+                .string()
+    });
+
+    return schema.validateAsync(data);
+}
+
+function loginValidation(data) {
+    const schema = Joi.object({
+        email: Joi
+                .string()
+                .min(4)
+                .email()
+                .required(),
+        password: Joi
+                .string()
+                .min(6)
+                .required()
+    });
+
+    return schema.validateAsync(data);
+}
+
+module.exports = { signupValidation, loginValidation };
