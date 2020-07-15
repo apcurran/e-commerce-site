b1efed272cdf0aa5700fb3d971b5ad2153edc564
Alex Curran <alexcurran.va@gmail.com>
2020-07-15 15:26:13 -0500

    Added stripe route for creation of payment intent

--- routes/checkout-router.js
+++ routes/checkout-router.js
@@ -4,6 +4,7 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../models/Product");
 const Cart = require("../models/Cart");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 router.get("/checkout-preview", (req, res) => {
     if (!req.session.cart) {
@@ -23,6 +24,21 @@ router.get("/checkout", (req, res) => {
     const cart = new Cart(req.session.cart);
 
     res.render("shop/checkout", { title: "Checkout", total: cart.totalPrice });
+});
+
+router.post("/api/create-payment-intent", async (req, res) => {
+    try {
+        const cart = new Cart(req.session.cart);
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: cart.totalPrice,
+          currency: "usd"
+        });
+    
+        res.send({ clientSecret: paymentIntent.client_secret });
+        
+    } catch (err) {
+        console.error(err);
+    }
 });
 
 module.exports = router;
