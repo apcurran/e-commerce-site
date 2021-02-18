367e8a6271f061d718e57be22feacfb19f126c9c
Alex Curran <alexcurran.va@gmail.com>
2021-02-18 10:50:49 -0600

    Added 500 server error page, also updated local dependencies

--- server.js
+++ server.js
@@ -80,9 +80,15 @@ app.use("/user", userRouter);
 app.use("/admin", adminRouter);
 app.use("/", checkoutRouter);
 
-// Final 404 route handler
+// 404 page
 app.use((req, res, next) => {
     res.status(404).render("404", { title: "404 Not Found" });
+});
+// Final catch-all error page
+app.use((err, req, res, next) => {
+    console.error(err);
+
+    res.status(500).render("error", { title: "Server Error", error: err.message });
 });
 
 app.listen(PORT, () => console.log(`Server running on port, ${PORT}.`));
