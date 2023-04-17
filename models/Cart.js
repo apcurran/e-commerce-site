d96b193b681f6befbea1e92f383a84da274d1a65
Alex Curran <alexcurran.va@gmail.com>
2023-04-17 13:55:23 -0500

    Work on new implementation to resolve cart total bug

--- models/Cart.js
+++ models/Cart.js
@@ -31,6 +31,7 @@
  * }} cart
  */
 
+// NEW IMPLEMENTATION
 /**
  * @param {cart|null} sessionCart pre-existing cart stored in session
  * @returns {cart}
@@ -121,7 +122,6 @@ function cartIncrementByOne(cart, productId) {
 }
 
 /**
- * 
  * @param {cart} cart 
  * @param {string} productId 
  * @returns {cart}
@@ -146,6 +146,138 @@ function cartDecrementByOne(cart, productId) {
 
     return cart;
 }
+
+/**
+ * @param {cartItem[]} cartItems 
+ * @returns {number}
+ */
+function calculateTotal(cartItems) {
+    let grandTotal = 0;
+
+    for (let cartItem of cartItems) {
+        const itemTotal = cartItem.itemQuantity * cartItem.itemDetails.price;
+        grandTotal += itemTotal;
+    }
+
+    return grandTotal;
+}
+
+// OLD IMPLEMENTATION
+// /**
+//  * @param {cart|null} sessionCart pre-existing cart stored in session
+//  * @returns {cart}
+//  */
+// function cartInitialize(sessionCart) {
+//     if (sessionCart) return sessionCart;
+
+//     return {
+//         cartItems: [],
+//         cartTotalQuantity: 0,
+//         cartTotalPrice: 0
+//     };
+// }
+
+// /**
+//  * @param {cart} cart
+//  * @param {product} productDetails
+//  * @param {string} productId
+//  * @returns {cart} modified cart
+//  */
+// function cartAddItem(cart, productDetails, productId) {
+//     let previousStoredItemIndex = cart.cartItems.findIndex((item) => String(item.itemDetails._id) === productId);
+
+//     if (previousStoredItemIndex === -1) {
+//         let product = {
+//             itemDetails: productDetails,
+//             itemQuantity: 1,
+//             itemTotalPrice: productDetails.price
+//         };
+//         cart.cartItems.push(product);
+//     } else {
+//         // update ITEM quantity
+//         cart.cartItems[previousStoredItemIndex].itemQuantity++;
+//         // update ITEM price
+//         const updatedItemTotal = cart.cartItems[previousStoredItemIndex].itemDetails.price * cart.cartItems[previousStoredItemIndex].itemQuantity;
+//         cart.cartItems[previousStoredItemIndex].itemTotalPrice = updatedItemTotal;
+//     }
+
+//     const storedItem = cart.cartItems.find((item) => String(item.itemDetails._id) === productId);
+//     // update CART quantity
+//     cart.cartTotalQuantity++;
+//     // update CART price
+//     cart.cartTotalPrice += storedItem.itemTotalPrice;
+
+//     return cart;
+// }
+
+// /**
+//  * 
+//  * @param {cart} cart 
+//  * @param {string} productId 
+//  * @returns {cart} modified cart after item removal
+//  */
+// function cartRemoveItem(cart, productId) {
+//     const storedItemIndex = cart.cartItems.findIndex((item) => String(item.itemDetails._id) === productId);
+//     const storedItem = cart.cartItems[storedItemIndex];
+
+//     if (storedItem === undefined) return cart;
+
+//     // update data
+//     cart.cartTotalQuantity -= storedItem.itemQuantity;
+//     cart.cartTotalPrice -= storedItem.itemTotalPrice;
+//     // remove item from cart
+//     cart.cartItems.splice(storedItemIndex, 1);
+
+//     return cart;
+// }
+
+// /**
+//  * @param {cart} cart
+//  * @param {string} productId 
+//  * @returns {cart}
+//  */
+// function cartIncrementByOne(cart, productId) {
+//     const storedItemIndex = cart.cartItems.findIndex((item) => String(item.itemDetails._id) === productId);
+//     const storedItem = cart.cartItems[storedItemIndex];
+
+//     if (storedItem === undefined) return cart;
+    
+//     // adjust item quantity and item total price
+//     cart.cartItems[storedItemIndex].itemQuantity++;
+//     cart.cartItems[storedItemIndex].itemTotalPrice += storedItem.itemDetails.price;
+//     // adjust total cart quantity and total cart price
+//     cart.cartTotalQuantity++;
+//     cart.cartTotalPrice += storedItem.itemDetails.price;
+
+//     return cart;
+// }
+
+// /**
+//  * 
+//  * @param {cart} cart 
+//  * @param {string} productId 
+//  * @returns {cart}
+//  */
+// function cartDecrementByOne(cart, productId) {
+//     const storedItemIndex = cart.cartItems.findIndex((item) => String(item.itemDetails._id) === productId);
+//     const storedItem = cart.cartItems[storedItemIndex];
+
+//     if (storedItem === undefined) return cart;
+
+//     // adjust item quantity and item total price
+//     cart.cartItems[storedItemIndex].itemQuantity--;
+//     cart.cartItems[storedItemIndex].itemTotalPrice -= storedItem.itemDetails.price;
+//     // adjust total cart quantity and total cart price
+//     cart.cartTotalQuantity--;
+//     cart.cartTotalPrice -= storedItem.itemDetails.price;
+
+//     if (storedItem.itemQuantity <= 0) {
+//         // remove item from cart
+//         cart.cartItems.splice(storedItemIndex, 1);
+//     }
+
+//     return cart;
+// }
 
 module.exports = {
     cartInitialize,
