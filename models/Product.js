461f74b68251f4bb6e12e91db1361154839b76de
Alex Curran <alexcurran.va@gmail.com>
2025-12-17 12:04:58 -0600

    Add enum data type with list of available options to Product schema, update add game and update game views in EJS

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,14 @@ const RatingsSchema = new mongoose.Schema({
 
 const ProductSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    genre: { type: String, required: true },
+    genre: {
+        type: String,
+        required: true,
+        enum: {
+            values: ["action", "adventure", "rpg", "sports", "horror", "fantasy"],
+            message: "{VALUE} is not a valid genre",
+        },
+    },
     price: { type: Number, min: 1, required: true },
     description: { type: String, required: true },
     img_path: { type: String, required: true },
